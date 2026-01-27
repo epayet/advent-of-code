8261f371e8958b8a8295ac162cd0a43693d5f290
manipayet <mani.payet@lyst.com>
2026-01-27 16:46:09 +0000

    day 1 part 2 solution

--- advent/twentytwentyfive/day1.py
+++ advent/twentytwentyfive/day1.py
@@ -13,3 +13,25 @@ def process_password_text(input: str) -> int:
         if curr == 0:
             nb_zero += 1
     return nb_zero
+
+
+def process_password_text_part2(input: str) -> int:
+    rotations = input.split("\n")
+    curr = 50
+    nb_zero = 0
+    for rotation in rotations:
+        direction = rotation[0]
+        amount = int(rotation[1:])
+        if direction == "L":
+            if curr - amount <= 0:
+                nb_zero += abs(curr - amount) // 100
+                # this is to know if we crossed 0 or not. it doesn't happen if we were already at 0
+                # note that this is left specific as on the right getting to 100 puts us back to 0
+                if curr > 0:
+                    nb_zero += 1
+            curr = (curr - amount) % 100
+        elif direction == "R":
+            if curr + amount >= 100:
+                nb_zero += (curr + amount) // 100
+            curr = (curr + amount) % 100
+    return nb_zero
--- tests/twentytwentyfive/test_day1.py
+++ tests/twentytwentyfive/test_day1.py
@@ -1,4 +1,4 @@
-from advent.twentytwentyfive.day1 import process_password_text
+from advent.twentytwentyfive.day1 import process_password_text, process_password_text_part2
 
 
 def test_example():
@@ -28,3 +28,45 @@ def test_less_than_100():
 R100"""
 
     assert process_password_text(input) == 2
+
+
+def test_part2():
+    input = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
+
+    assert process_password_text_part2(input) == 6
+
+
+def test_part2_left():
+    input = """L1000"""
+    assert process_password_text_part2(input) == 10
+
+
+def test_part2_right():
+    input = """R1000"""
+    assert process_password_text_part2(input) == 10
+
+
+def test_part2_left_exact():
+    input = """L50"""
+    assert process_password_text_part2(input) == 1
+
+
+def test_part2_right_exact():
+    input = """R50"""
+    assert process_password_text_part2(input) == 1
+
+
+def test_part2_right_exact_back_to0():
+    input = """R50
+L100
+R100"""
+    assert process_password_text_part2(input) == 3
